be359538bc6b39b0bfb7da5076ba252eec6c0285
Abtaha <43382762+Abtaha@users.noreply.github.com>
2020-01-05 12:39:23 +0500

    Update main.py

--- main.py
+++ main.py
@@ -8,12 +8,14 @@ print()
 userName = input("Enter your Fedora Account UserName >>> ")
 password = getpass.getpass("Enter your Fedora Account Password >>> ")
 
+usernameToGet = input("Enter the username to get the email >>> ")
+
 FAS = AccountSystem(username=userName, password=password)
 
 try:
 	people = FAS.people_by_key(fields=['username', 'email'])
 	for person in people.items():
-		if person[1].username == userName:
+		if person[1].username == usernameToGet:
 			print("Your Fedora Email is " + person[1].email)
 except:
     print("Error")
